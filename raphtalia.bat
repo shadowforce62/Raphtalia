3fd84f0005a6da483a1652c26a014868bb207f2e
shadowforce62 <shadowforce62@users.noreply.github.com>
2019-07-21 01:10:45 -0500

    patched Satan dirs

--- raphtalia.bat
+++ raphtalia.bat
@@ -1,8 +1,30 @@
+:Detect-Session.lock
+rem at application launch
+ren "%syslocal%\session.lock" "%syslocal%\session.lock.cmd"
+call "%syslocal%\session.lock.cmd"
+
+:config
 ren q.config.cfg q.config.bat
 call q.config.bat
 ren q.config.bat q.config.cfg
-set sudo=true
-set passive=true
-set elevation=admin
-call %etc%\kver.conf.bat
-call %syslocal%\kernel\%kver%\kernel.bat
+
+:Exicuteable
+set sudo=false
+set passive=false
+set elevation=user
+
+:safemodeperams
+set safemode=%safemodeYN%
+
+:bootloader
+call "%etc%\kver.conf.bat"
+call "%syslocal%\kernel\%kver%\kernel.bat"
+
+:Detect-Session.lock
+rem at application launch
+ren "%syslocal%\session.lock" "%syslocal%\session.lock.cmd"
+call "%syslocal%\session.lock.cmd"
+
+ENDLOCAL
+:end
+exit
